eb5ffe354ff262710a21de523d2bf19a91354e9f
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-04-20 22:31:37 +0800

    Update main.py

--- imagenet/main.py
+++ imagenet/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import os
 import random
@@ -16,8 +17,26 @@ import torch.utils.data
 import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from multiprocessing import set_start_method
+#import matplotlib.pyplot as plt
 #import torchvision.models as models
-import Resnet.vision.torchvision.models as models
+
+import sys
+sys.path.insert(1,'/scratch/helenr6/vision/torchvision')
+import models
+
+# sys.path.append('/vision/torchvision/models')
+#import models as models
+#import models
+#import vision.torchvision as torchvision
+# import os
+# os.chdir('/scratch/helenr6/vision/torchvision')
+# print("Current working directory: {0}".format(os.getcwd()))
+#import models
+#from helenr6.vision.torchvision import models
+#from vision.torchvision import models
+#import torchvision.models as models 
+#import vision.torchvision.models as models
 from torch.utils.data import Dataset 
 from pytorch_pretrained_biggan import (BigGAN, one_hot_from_names, truncated_noise_sample,
                                        save_as_images, convert_to_images,display_in_terminal)
@@ -34,24 +53,28 @@ import imageio
 import random
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
+print("hello world")
+#resnet=models.resnet50()
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
-parser.add_argument('data', metavar='DIR',
-                    help='path to dataset')
+# parser.add_argument('data', metavar='DIR',
+#                     help='path to dataset')
 parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                     choices=model_names,
                     help='model architecture: ' +
                         ' | '.join(model_names) +
                         ' (default: resnet18)')
-parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                    help='number of data loading workers (default: 4)')
+# parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+#                     help='number of data loading workers (default: 4)')
+parser.add_argument('-j', '--workers', default=1, type=int, metavar='N',
+                    help='number of data loading workers (default: 1)')
 # parser.add_argument('--epochs', default=90, type=int, metavar='N',
 #                     help='number of total epochs to run')
-parser.add_argument('--epochs', default=1, type=int, metavar='N',
+parser.add_argument('--epochs', default=10, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
@@ -60,9 +83,9 @@ parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
 #                     help='mini-batch size (default: 256), this is the total '
 #                          'batch size of all GPUs on the current node when '
 #                          'using Data Parallel or Distributed Data Parallel')
-parser.add_argument('-b', '--batch-size', default=6, type=int,
+parser.add_argument('-b', '--batch-size', default=128, type=int,
                     metavar='N',
-                    help='mini-batch size (default: 6), this is the total '
+                    help='mini-batch size (default: 128), this is the total '
                          'batch size of all GPUs on the current node when '
                          'using Data Parallel or Distributed Data Parallel')
 parser.add_argument('-t','--temp', default=1, type=int, metavar='T',
@@ -101,11 +124,18 @@ parser.add_argument('--multiprocessing-distributed', action='store_true',
                          'multi node data parallel training')
 
 best_acc1 = 0
-
+# args = parser.parse_args()
+# model = models.__dict__[args.arch]()
+# for (name, layer) in model._modules.items():
+#     #iteration over outer layers
+#     print((name, layer))
+# if args.gpu is not None:
+#     print("hello gpu")
+#     warnings.warn('You have chosen a specific GPU. This will completely ')
 
 def main():
     args = parser.parse_args()
-
+    print("Current working directory: {0}".format(os.getcwd()))
     if args.seed is not None:
         random.seed(args.seed)
         torch.manual_seed(args.seed)
@@ -183,8 +213,10 @@ def main_worker(gpu, ngpus_per_node, args):
             # available GPUs if device_ids are not set
             model = torch.nn.parallel.DistributedDataParallel(model)
     elif args.gpu is not None:
+        #torch.cuda.set_device(args.gpu)
         torch.cuda.set_device(args.gpu)
-        model = model.cuda(args.gpu)
+        # model = model.cuda(args.gpu)
+        model = model.cuda()
     else:
         # DataParallel will divide and allocate batch_size to all available GPUs
         if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
@@ -200,6 +232,7 @@ def main_worker(gpu, ngpus_per_node, args):
         loss_fn_z=nn.MSELoss()(z,z_hat)
         total_loss=loss_fn_class+loss_fn_z
         return total_loss
+        #return torch.norm(class_vector-class_hat) +torch.norm(z-z_hat)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
@@ -229,20 +262,20 @@ def main_worker(gpu, ngpus_per_node, args):
 
     cudnn.benchmark = True
 
-    # Data loading code
-    traindir = os.path.join(args.data, 'train')
-    valdir = os.path.join(args.data, 'val')
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                     std=[0.229, 0.224, 0.225])
+    # # Data loading code
+    # traindir = os.path.join(args.data, 'train')
+    # valdir = os.path.join(args.data, 'val')
+    # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+    #                                  std=[0.229, 0.224, 0.225])
 
-    train_dataset = datasets.ImageFolder(
-        traindir,
-        transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize,
-        ]))
+    # train_dataset = datasets.ImageFolder(
+    #     traindir,
+    #     transforms.Compose([
+    #         transforms.RandomResizedCrop(224),
+    #         transforms.RandomHorizontalFlip(),
+    #         transforms.ToTensor(),
+    #         normalize,
+    #     ]))
 
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
@@ -253,21 +286,22 @@ def main_worker(gpu, ngpus_per_node, args):
     #     train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),
     #     num_workers=args.workers, pin_memory=True, sampler=train_sampler)
 
-    val_loader = torch.utils.data.DataLoader(
-        datasets.ImageFolder(valdir, transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            normalize,
-        ])),
-        batch_size=args.batch_size, shuffle=False,
-        num_workers=args.workers, pin_memory=True)
+    # val_loader = torch.utils.data.DataLoader(
+    #     datasets.ImageFolder(valdir, transforms.Compose([
+    #         transforms.Resize(256),
+    #         transforms.CenterCrop(224),
+    #         transforms.ToTensor(),
+    #         normalize,
+    #     ])),
+    #     batch_size=args.batch_size, shuffle=False,
+    #     num_workers=args.workers, pin_memory=True)
 
     if args.evaluate:
         validate(val_loader, model, criterion, args)
         return
     truncation=0.5
     GAN_model = BigGAN.from_pretrained('biggan-deep-256')
+    GAN_model.cuda(args.gpu)
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
@@ -277,17 +311,21 @@ def main_worker(gpu, ngpus_per_node, args):
         image_list=[]
         class_list=[]
         z_list=[]
+        m = nn.Softmax(dim=1)
         for b in range(args.batch_size):
             z_np = truncated_normal((1, 128), low=-2, high=2)
+            #z=Variable(torch.from_numpy(z_np), requires_grad=True).cuda(args.gpu).detach().requires_grad_(True)
             z=Variable(torch.from_numpy(z_np), requires_grad=True).cuda(args.gpu).detach()
-            class_vector = torch.FloatTensor(1, 1000).zero_()
-            class_vector[:, random.randint(0, 999)] = 1
+            input = torch.randn(1, 1000)
+            #class_vector = torch.FloatTensor(1, 1000).zero_()
+            #class_vector[:, random.randint(0, 999)] = 1
+            class_vector=m(input).cuda(args.gpu).detach()
             start_point=GAN_model(z,class_vector,truncation)
-            image_list.append(start_point)
+            image_list.append(start_point.detach())
             z_list.append(z)
             class_list.append(class_vector)
         train_dataset = GANDataset(z_list,image_list,class_list)
-        train_loader=DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),num_workers=args.workers, pin_memory=True)
+        train_loader=torch.utils.data.DataLoader(dataset=train_dataset, batch_size=args.batch_size, shuffle=(train_sampler is None),num_workers=args.workers, pin_memory=False)
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch, args)
 
@@ -299,16 +337,16 @@ def main_worker(gpu, ngpus_per_node, args):
         # best_acc1 = max(acc1, best_acc1)
         is_best=True
         best_acc1=0
-        if not args.multiprocessing_distributed or (args.multiprocessing_distributed
-                and args.rank % ngpus_per_node == 0):
-            save_checkpoint({
-                'epoch': epoch + 1,
-                'arch': args.arch,
-                'state_dict': model.state_dict(),
-                'best_acc1': best_acc1,
-                'optimizer' : optimizer.state_dict(),
-                'T': args.temp(),
-            }, is_best)
+        # if not args.multiprocessing_distributed or (args.multiprocessing_distributed
+        #         and args.rank % ngpus_per_node == 0):
+        #     save_checkpoint({
+        #         'epoch': epoch + 1,
+        #         'arch': args.arch,
+        #         'state_dict': model.state_dict(),
+        #         'best_acc1': best_acc1,
+        #         'optimizer' : optimizer.state_dict(),
+        #         'T': args.temp(),
+        #     }, is_best)
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args):
@@ -317,6 +355,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
     losses = AverageMeter('Loss', ':.4e')
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
+    loss_list=[]
     progress = ProgressMeter(
         len(train_loader),
         [batch_time, data_time, losses, top1, top5],
@@ -327,7 +366,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
     end = time.time()
     #for i, (images, target) in enumerate(train_loader):
-    for i, (images, target_z,target_class_vector) in enumerate(train_loader):
+    for i, (target_z,target_class_vector,images) in enumerate(train_loader):
         # measure data loading time
         data_time.update(time.time() - end)
 
@@ -340,17 +379,22 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
         # compute output
         #output = model(images)
-        z,class_vector= model(images)
+        images=images.squeeze(dim=1)
+        input_to_resnet=F.interpolate(images,(224,224))
+        z,class_vector= model(input_to_resnet)
         #loss = criterion(output, target)
         T=args.temp
+        target_z=target_z.squeeze()
+        target_class_vector=target_class_vector.squeeze()
         loss = criterion(target_z,target_class_vector,z,class_vector,T)
 
         # # measure accuracy and record loss
         # acc1, acc5 = accuracy(output, target, topk=(1, 5))
-        # losses.update(loss.item(), images.size(0))
+        losses.update(loss.item(), images.size(0))
         # top1.update(acc1[0], images.size(0))
         # top5.update(acc5[0], images.size(0))
 
+        loss_list.append(loss.item())
         # compute gradient and do SGD step
         optimizer.zero_grad()
         loss.backward()
@@ -362,58 +406,60 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
 
         if i % args.print_freq == 0:
             progress.display(i)
+    #plt.plot(loss_list)
+    print(loss_list)
 
 
-def validate(val_loader, model, criterion, args):
-    batch_time = AverageMeter('Time', ':6.3f')
-    losses = AverageMeter('Loss', ':.4e')
-    top1 = AverageMeter('Acc@1', ':6.2f')
-    top5 = AverageMeter('Acc@5', ':6.2f')
-    progress = ProgressMeter(
-        len(val_loader),
-        [batch_time, losses, top1, top5],
-        prefix='Test: ')
+# def validate(val_loader, model, criterion, args):
+#     batch_time = AverageMeter('Time', ':6.3f')
+#     losses = AverageMeter('Loss', ':.4e')
+#     top1 = AverageMeter('Acc@1', ':6.2f')
+#     top5 = AverageMeter('Acc@5', ':6.2f')
+#     progress = ProgressMeter(
+#         len(val_loader),
+#         [batch_time, losses, top1, top5],
+#         prefix='Test: ')
 
-    # switch to evaluate mode
-    model.eval()
+#     # switch to evaluate mode
+#     model.eval()
 
-    with torch.no_grad():
-        end = time.time()
-        for i, (images, target) in enumerate(val_loader):
-            if args.gpu is not None:
-                images = images.cuda(args.gpu, non_blocking=True)
-            if torch.cuda.is_available():
-                target = target.cuda(args.gpu, non_blocking=True)
+#     with torch.no_grad():
+#         end = time.time()
+#         for i, (images, target) in enumerate(val_loader):
+#             if args.gpu is not None:
+#                 images = images.cuda(args.gpu, non_blocking=True)
+#             if torch.cuda.is_available():
+#                 target = target.cuda(args.gpu, non_blocking=True)
 
-            # compute output
-            output = model(images)
-            #z,class_vector= model(images)
-            loss = criterion(output, target)
+#             # compute output
+#             output = model(images)
+#             #z,class_vector= model(images)
+#             loss = criterion(output, target)
 
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1[0], images.size(0))
-            top5.update(acc5[0], images.size(0))
+#             # measure accuracy and record loss
+#             acc1, acc5 = accuracy(output, target, topk=(1, 5))
+#             losses.update(loss.item(), images.size(0))
+#             top1.update(acc1[0], images.size(0))
+#             top5.update(acc5[0], images.size(0))
 
-            # measure elapsed time
-            batch_time.update(time.time() - end)
-            end = time.time()
+#             # measure elapsed time
+#             batch_time.update(time.time() - end)
+#             end = time.time()
 
-            if i % args.print_freq == 0:
-                progress.display(i)
+#             if i % args.print_freq == 0:
+#                 progress.display(i)
 
-        # TODO: this should also be done with the ProgressMeter
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+#         # TODO: this should also be done with the ProgressMeter
+#         print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+#               .format(top1=top1, top5=top5))
 
-    return top1.avg
+#     return top1.avg
 
 
-def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'model_best.pth.tar')
+# def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+#     torch.save(state, filename)
+#     if is_best:
+#         shutil.copyfile(filename, 'model_best.pth.tar')
 def truncated_normal(size, low=0, high=1):
     values = truncnorm.rvs(low, high, size=size).astype(np.float32)
     return values
@@ -479,21 +525,22 @@ def adjust_learning_rate(optimizer, epoch, args):
         param_group['lr'] = lr
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the accuracy over the k top predictions for the specified values of k"""
-    with torch.no_grad():
-        maxk = max(topk)
-        batch_size = target.size(0)
+# def accuracy(output, target, topk=(1,)):
+#     """Computes the accuracy over the k top predictions for the specified values of k"""
+#     with torch.no_grad():
+#         maxk = max(topk)
+#         batch_size = target.size(0)
 
-        _, pred = output.topk(maxk, 1, True, True)
-        pred = pred.t()
-        correct = pred.eq(target.view(1, -1).expand_as(pred))
+#         _, pred = output.topk(maxk, 1, True, True)
+#         pred = pred.t()
+#         correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-        res = []
-        for k in topk:
-            correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-            res.append(correct_k.mul_(100.0 / batch_size))
-        return res
+#         res = []
+#         for k in topk:
+#             correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+#             res.append(correct_k.mul_(100.0 / batch_size))
+#         return res
 
 if __name__ == '__main__':
+    set_start_method('spawn')
     main()
