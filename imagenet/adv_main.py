c5bef509d617ed6c8deca16273b8475561b5e6f6
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-10-23 09:41:01 -0400

    Update adv_main.py

--- imagenet/adv_main.py
+++ imagenet/adv_main.py
@@ -547,7 +547,7 @@ def validate(val_loader, model, criterion, args):
         # targeted=False)
         adversary = L2PGDAttack(
         model, loss_fn=nn.CrossEntropyLoss(reduction="sum"), eps=3.0,
-        nb_iter=40, eps_iter=0.01, rand_init=True, clip_min=0.0, clip_max=1.0,
+        nb_iter=20, eps_iter=0.375, rand_init=True, clip_min=0.0, clip_max=1.0,
         targeted=False)
         end = time.time()
         print("enumerate dataloader")
