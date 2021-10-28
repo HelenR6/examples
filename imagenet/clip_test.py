bb6144a0d3776744690919b5d5b9766f0aba6e23
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-10-27 20:23:26 -0400

    Update clip_test.py

--- imagenet/clip_test.py
+++ imagenet/clip_test.py
@@ -4,3 +4,20 @@ import numpy as np
 import torch
 import clip
 from tqdm.notebook import tqdm
+
+def zeroshot_classifier(classnames, templates):
+    with torch.no_grad():
+        zeroshot_weights = []
+        for classname in tqdm(classnames):
+            texts = [template.format(classname) for template in templates] #format with class
+            texts = clip.tokenize(texts).cuda() #tokenize
+            class_embeddings = model.encode_text(texts) #embed with text encoder
+            class_embeddings /= class_embeddings.norm(dim=-1, keepdim=True)
+            class_embedding = class_embeddings.mean(dim=0)
+            class_embedding /= class_embedding.norm()
+            zeroshot_weights.append(class_embedding)
+        zeroshot_weights = torch.stack(zeroshot_weights, dim=1).cuda()
+    return zeroshot_weights
+
+
+zeroshot_weights = zeroshot_classifier(imagenet_classes, imagenet_templates)
