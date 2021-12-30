e9092102243c5698a0eb409e6c88858d8b91b0ae
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2021-12-30 08:49:08 -0500

    Update adv_main.py

--- imagenet/adv_main.py
+++ imagenet/adv_main.py
@@ -533,26 +533,6 @@ def load_model(model_type):
     ])
     return resnet
 
-  if model_type=="v_resnet_0":
-    resnet=models.resnet50(pretrained=False)
-    checkpoint = torch.load('/content/gdrive/MyDrive/model_checkpoints/model_epoch0.pth.tar',map_location=torch.device('cuda') )
-    state_dict=checkpoint['state_dict']
-    for k in list(state_dict.keys()):
-        if k.startswith('module.') :
-
-            state_dict[k[len('module.'):]] = state_dict[k]
-        del state_dict[k]
-    resnet.load_state_dict(state_dict)
-    preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(
-    mean=[0.485, 0.456, 0.406],
-    std=[0.229, 0.224, 0.225])
-    ])
-    return resnet
-
   if model_type=="v_resnet_90":
     resnet=models.resnet50(pretrained=False)
     checkpoint = torch.load('/content/gdrive/MyDrive/model_checkpoints/model_epoch90.pth.tar',map_location=torch.device('cuda') )
