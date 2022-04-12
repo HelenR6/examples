b8dab9018c4afe98b3d9f4a8db7c73bc566c3b57
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-04-12 09:21:37 -0400

    Update load_model.py

--- imagenet/load_model.py
+++ imagenet/load_model.py
@@ -4,6 +4,23 @@ import matplotlib.pyplot as plt
 from torchvision import transforms 
 
 def load_model(model_type):
+  if model_type=="resnet50_trained_on_SIN" or model_type=="resnet50_trained_on_SIN_and_IN" or model_type=="resnet50_trained_on_SIN_and_IN_then_finetuned_on_IN":
+    state_dict = torch.load(f'/content/gdrive/MyDrive/model_checkpoints/{model_type}.pth.tar',map_location="cpu")['state_dict']
+    resnet=models.resnet50(pretrained=False)
+    for k in list(state_dict.keys()):
+        if k.startswith('module'):
+            state_dict[k[len("module."):]] = state_dict[k]
+        del state_dict[k]
+    resnet.load_state_dict(state_dict)
+    preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+    mean=[0.485, 0.456, 0.406],
+    std=[0.229, 0.224, 0.225])
+    ])
+    
   if model_type=="simclr":
     # load checkpoint for simclr
     checkpoint = torch.load('/content/gdrive/MyDrive/resnet50-1x.pth')
