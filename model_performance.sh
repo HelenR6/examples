c945fdcf1dce1a66da5085905c04d000673a938a
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-04-12 10:03:40 -0400

    Create model_performance.sh

---
+++ model_performance.sh
@@ -0,0 +1,12 @@
+allAttacks=("l2_0.15","l2_3","linf1_1020","linf4_255")
+for model_name in "$@"
+do
+    for attack in allAttacks
+        do
+            python examples/imagenet/adv_main.py -a "$model_name" --attack "$attack" -b 64 --evaluate --pretrained /content/gdrive/MyDrive/imagenet
+        done
+    python imagenet-r/eval.py "$model_name"
+    python natural-adv-examples/eval.py "$model_name"
+    python examples/imagenet/sketch_main.py -a st_resnet -b 64 --evaluate --pretrained /content/gdrive/MyDrive/sketch
+
+done
