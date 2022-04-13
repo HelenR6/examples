da14724437b6207b308eac0d8dae00c07ed4a777
Helen_Ren <59241451+HelenR6@users.noreply.github.com>
2022-04-13 12:30:53 -0400

    Create env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,5 @@
+!pip install git+https://github.com/HelenR6/robustness
+!pip install git+https://github.com/HelenR6/advertorch
+!git clone https://github.com/HelenR6/examples
+!git clone https://github.com/HelenR6/imagenet-r
+!git clone https://github.com/HelenR6/natural-adv-examples
